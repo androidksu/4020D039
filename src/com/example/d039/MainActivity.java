401a888d92b86c5e6c06dd4c9f9650e3306413aa
4020D039 <royedfa5229@hotmail.com>
2014-12-26 13:29:48 +0800

    first commit

--- src/com/example/d039/MainActivity.java
+++ src/com/example/d039/MainActivity.java
@@ -1,5 +1,5 @@
 package com.example.d039;
-
+//5555
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -22,6 +22,7 @@ public void sendMessage(View view){
 	Intent intent = new Intent( this,DisplayMessageActivity.class);
 	EditText editText = (EditText) findViewById(R.id.edit_message);
 	String message = editText.getText().toString();
+	Intent intet;
 	intet.putExtra(EXTRA_MESSAGE,message);
 	startActivity(intent);
 
